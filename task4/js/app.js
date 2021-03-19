630f7d79549fa4ac30dbceeca0cc7581fa6f8f77
Viktor <chelweb18@gmail.com>
2021-03-19 23:19:35 +0500

    Complete fourth task

---
+++ task4/js/app.js
@@ -0,0 +1,20 @@
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+console.log('addition:', addition(4,5));
+console.log('subtraction:', subtraction(4,5));
+console.log('multiplication:', multiplication(4,5));
+console.log('division:', division(4,5));
